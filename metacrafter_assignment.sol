6d64fede96cfc236ade5eb3bd0ef38346c8ca0c3
yanema2001 <148040962+yanema2001@users.noreply.github.com>
2023-10-19 20:11:22 +0530

    Update metacrafter_assignment.sol

--- metacrafter_assignment.sol
+++ metacrafter_assignment.sol
@@ -15,8 +15,8 @@ pragma solidity 0.8.18;
 */
 contract MyToken {
     // public variables here
-    string public tokenname = "ADARSH"; 
-    string public tokenAbbrv = "ADR"; 
+    string public tokenname = "YASH"; 
+    string public tokenAbbrv = "YAS"; 
     uint public totalSupply = 0;
     // mapping variable here
     mapping(address => uint) public balances;
